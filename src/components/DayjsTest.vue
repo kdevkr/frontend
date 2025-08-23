f7ff9a7adee0a4b9542173f51b4c51c27b0d039d
Mambo <kdevkr@gmail.com>
2025-08-23 22:08:28 +0900

    Enhance `DayjsTest.vue` with computed properties and additional Day.js methods
    fix missing `<body>` tag in `index.html`.

--- src/components/DayjsTest.vue
+++ src/components/DayjsTest.vue
@@ -1,17 +1,23 @@
 <script setup>
-import { ref } from 'vue'
+import {computed, ref} from 'vue'
 import dayjs from "dayjs";
 
-const date = ref(dayjs())
-date.value = dayjs('20250823')
-
-console.log('locale:', dayjs.locale())
-console.log('tz: ', dayjs.tz().format())
-console.log('fromNow: ', dayjs().fromNow())
+const date = ref(dayjs().format('YYYY-MM-DD'))
+const date_ = computed(() => dayjs(date.value))
 </script>
 
 <template>
-{{ date.format() }}
+  <div><q-date v-model="date" mask="YYYY-MM-DD" no-unset /></div>
+  <div class="q-pa-lg text-lg">
+    <p>Date: {{ date }}</p>
+    <ol>
+      <li>format(): {{date_.format()}}</li>
+      <li>toISOString(): {{date_.toISOString()}}</li>
+      <li>valueOf(): {{date_.valueOf()}}</li>
+      <li>from(): {{date_.from()}}</li>
+    </ol>
+  </div>
+
 </template>
 
 <style scoped></style>
